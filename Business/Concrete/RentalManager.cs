9eb8416763309f10228f5657e06d83b298895ca6
Recep Orkun Çevik <receporkuncevik@gmail.com>
2021-02-17 00:32:04 +0300

    Veritabanına eklenen tablolar için entityler oluşturuldu CRUD operasyonları yazıldı.

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private readonly IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            if(rental.ReturnDate == null)
+            {
+                return new Result(false,"Araç Kirada Olduğundan Bu Araç Kiraya Verilemez.");
+            }
+            _rentalDal.Add(rental);
+            return new Result(true, "Araç başarıyla kiraya verildi");
+            
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new Result(true);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.Id == rentalId));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new Result(true);
+        }
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new Result(true, "Kullanıcı Başarıyla eklendi.");
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new Result(true, "Kullanıcı Başarıyla silindi.");
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll(),"Kullanıcılar Başarıyla Listelendi.");
+        }
+
+        public IDataResult<User> GetById(int userId)
+        {
+            return new SuccessDataResult<User>(_userDal.Get(u => u.Id == userId),"İstenilen Kullanıcı Başarıyla getirildi.");
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new Result(true, "Kullanıcı Başarıyla güncellendi.");
+        }
+    }
+}
