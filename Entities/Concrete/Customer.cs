fe055eb08fbbe268902fa8ed25258d834638d7d3
Recep Orkun Çevik <receporkuncevik@gmail.com>
2021-02-17 15:05:17 +0300

    WebApi entegrasyonu yapıldı Service'lerin api tarafı yazıldı.

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -1,9 +1,11 @@
 ﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities.Concrete
 {
     public class Customer : IEntity
     {
+        [Key]
         public int UserId { get; set; }
         public string CompanyName { get; set; }
     }
